884f8c50ca7b0fcf79ae708a843b152d5908ac27
Reese Lee <reese.lee.cy@gmail.com>
2019-03-29 11:35:02 -0700

    add initial js variables to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    var track1 = $("input:radio[name=qOne]:checked").val();
+    var track2 = $("input:radio[name=qTwo]:checked").val();
+    var track3 = $("input:radio[name=qThree]:checked").val();
+
+    
+
+
+
+  })
+
+
+
+
+})
