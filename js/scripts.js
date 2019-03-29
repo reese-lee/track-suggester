8b1a88e96b36f5f41936a3b8a5c461e56be5edfe
Reese Lee <reese.lee.cy@gmail.com>
2019-03-29 14:34:17 -0700

    edit variables in JS to see if a new function will work to pull the results

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,38 @@
 $(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-    var qOne = parseInt($("#qOne").val());
-    var qTwo = parseInt($("#qTwo").val());
-    var qThree = parseInt($("#qThree").val());
-    var qFour = parseInt($("#qFour").val());
-    var qFive = parseInt($("#qFive").val());
+    // var qOne = parseInt($("#qOne").val());
+    // var qTwo = parseInt($("#qTwo").val());
+    // var qThree = parseInt($("#qThree").val());
+    // var qFour = parseInt($("#qFour").val());
+    // var qFive = parseInt($("#qFive").val());
 
-    if(qOne === 1 && qTwo === 1 && qThree === 1 && qFour === 1 && qFive ===1) {
-      console.log("ayo");
+    var qOne = $("input:radio[name=qOne]:checked").val();
+    var qTwo = $("input:radio[name=qTwo]:checked").val();
+    var qThree = $("input:radio[name=qThree]:checked").val();
+    var qFour = $("input:radio[name=qFour]:checked").val();
+    var qFive = $("input:radio[name=qFive]:checked").val();
+
+    if(qOne = ) {
       $("#optCD").show();
       $("#optPD").hide();
       $("#optJA").hide();
       $("#workshop").hide();
-    } else if(qOne === 2 && qTwo === 2 && qThree === 2 && qFour === 1 && qFive === 2)
+    } else if(total >= 10) {
         $("#optPD").show();
         $("#optCD").hide();
         $("#optJA").hide();
         $("#workshop").hide();
-    } else if(qOne === 3 && qTwo === 3 && qThree === 3 && qFour === 3 && qFive === 3) {
+    } else if(total >=15) {
         $("#optJA").show();
         $("#optPD").hide();
         $("#optCD").hide();
         $("#workshop").hide();
     } else {
-        $("#workshop").show();
-        $("#optCD").hide();
-        $("#optPD").hide();
-        $("#optJA").hide();
+      $("#workshop").show();
+      $("#optCD").hide();
+      $("#optPD").hide();
+      $("#optJA").hide();
 
       };
 
