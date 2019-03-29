956fa24aae7a5cf26bc7afb2d444773ee94ead8f
Reese Lee <reese.lee.cy@gmail.com>
2019-03-29 13:41:03 -0700

    edit js function to see if it will work with the HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-  $("#form").submit(function(event) {
+$(function() {
+  $("form#survey").submit(function(event) {
     event.preventDefault();
     var qOne = parseInt($("#qOne").val());
     var qTwo = parseInt($("#qTwo").val());
@@ -7,25 +7,31 @@ $(document).ready(function () {
     var qFour = parseInt($("#qFour").val());
     var qFive = parseInt($("#qFive").val());
 
-    if(qOne ===  ) {
+    if(qOne === 1 && qTwo === 1 && qThree === 1 && qFour === 1 && qFive ===1) {
+      console.log("ayo");
       $("#optCD").show();
       $("#optPD").hide();
       $("#optJA").hide();
-    } else if (qOne ===) {
-      $("#optCD").show();
-      $("#optPD").hide();
-      $("#optJA").hide();
-    } else if (qOne ===) {
-      $("#optCD").show();
-      $("#optPD").hide();
-      $("#optJA").hide();
-    } 
+      $("#workshop").hide();
+    } else if(qOne === 2 && qTwo === 2 && qThree === 2 && qFour === 1 && qFive === 2)
+        $("#optPD").show();
+        $("#optCD").hide();
+        $("#optJA").hide();
+        $("#workshop").hide();
+    } else if(qOne === 3 && qTwo === 3 && qThree === 3 && qFour === 3 && qFive === 3) {
+        $("#optJA").show();
+        $("#optPD").hide();
+        $("#optCD").hide();
+        $("#workshop").hide();
+    } else {
+        $("#workshop").show();
+        $("#optCD").hide();
+        $("#optPD").hide();
+        $("#optJA").hide();
+
+      };
 
 
 
-  })
-
-
-
-
-})
+});
+  });
