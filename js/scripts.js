923615402e3aa40d7a1d46be9886429606723f7e
Reese Lee <reese.lee.cy@gmail.com>
2019-03-29 14:49:00 -0700

    add else if and values and variables that WORKgit add js/scripts.jsgit add js/scripts.jsgit add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,17 @@ $(function() {
     var qFour = $("input:radio[name=qFour]:checked").val();
     var qFive = $("input:radio[name=qFive]:checked").val();
 
-    if(qOne = ) {
+    if(qOne === "cd1" && qTwo === "cd2" && qThree === "cd3" && qFour === "cd4" && qFive === "cd5") {
       $("#optCD").show();
       $("#optPD").hide();
       $("#optJA").hide();
       $("#workshop").hide();
-    } else if(total >= 10) {
+    } else if(qOne === "pd1" && qTwo === "pd2" && qThree === "pd3" && qFour === "cd4" && qFive === "pd5") {
         $("#optPD").show();
         $("#optCD").hide();
         $("#optJA").hide();
         $("#workshop").hide();
-    } else if(total >=15) {
+    } else if(qOne === "ja1" && qTwo === "ja2" && qThree === "ja3" && qFour === "ja4" && qFive === "ja5") {
         $("#optJA").show();
         $("#optPD").hide();
         $("#optCD").hide();
